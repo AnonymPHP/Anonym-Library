9bed553d27008ca8cbbc46c7eca952e00dc4eeea
anonymframework <serif.saglam119@gmail.com>
2015-08-21 04:57:13 +0300

    update

--- Build/subsplit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git subsplit init https://github.com/AnonymPHP/Anonym-Library.git
-
-git subsplit publish --heads="master" --no-tags src/Anonym/Patterns:git@github.com:AnonymPHP/patterns.git
