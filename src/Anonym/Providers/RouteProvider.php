1a64d225a182517296bc6d1f731fe4803ee2e8b3
anonymframework <serif.saglam119@gmail.com>
2015-08-22 01:28:06 +0300

    added route provider

---
+++ src/Anonym/Providers/RouteProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Providers;
+
+
+use Anonym\Bootstrap\ServiceProvider;
+
+class RouteProvider extends ServiceProvider
+{
+
+}
