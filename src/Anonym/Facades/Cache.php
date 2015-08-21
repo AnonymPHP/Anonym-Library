db1b5f06fed8e1e934e4dbeaa52db33fe34162ac
anonymframework <serif.saglam119@gmail.com>
2015-08-21 23:55:47 +0300

    added getFacadeClass() method

--- src/Anonym/Facades/Cache.php
+++ src/Anonym/Facades/Cache.php
@@ -12,8 +12,22 @@ namespace Anonym\Facades;
 
 
 use Anonym\Patterns\Facade;
+use Anonym\Components\Cache\DriverInterface;
 
 class Cache extends Facade
 {
+
+    /**
+     *  get the cache facade
+     *
+     * @return DriverInterface
+     */
+    protected static function getFacadeClass()
+    {
+        $configs = Config::get('stroge.cache');
+        $driver = isset($configs['driver']) ? $configs['driver'] : '';
+
+        return (new \Anonym\Components\Cache\Cache())->driver($driver, $configs);
+    }
 
 }
