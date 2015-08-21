2eef86410409a1240aa55c448cf9e26acdc04eef
anonymframework <serif.saglam119@gmail.com>
2015-08-22 01:32:32 +0300

    made some updates

--- src/Anonym/Constructors/RequestConstructor.php
+++ src/Anonym/Constructors/RequestConstructor.php
@@ -12,15 +12,33 @@ namespace Anonym\Constructors;
 
 
 use Anonym\Bootstrap\Bootstrap;
+use Anonym\Components\HttpClient\Request;
 
 class RequestConstructor
 {
 
     /**
+     * add the request  and response to container
+     *
      * @param Bootstrap $app
      */
     public function __construct(Bootstrap $app)
     {
 
+        // register the request
+        $app->bind(
+            'http.request',
+            function () {
+                return new Request();
+            }
+        );
+
+        // register the response
+        $app->bind(
+            'http.response',
+            function () use ($app) {
+                return $app->make('http.request')->getResponse();
+            }
+        );
     }
 }
