09018c0a4346ac2b0d0aa7df92d397a257ea2b01
anonymframework <serif.saglam119@gmail.com>
2015-08-21 21:08:49 +0300

    added stroge.php

---
+++ src/Anonym/Facades/Stroge.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Facades;
+
+/**
+ * Class Filesystem
+ * @package Anonym\Facades
+ */
+class Stroge
+{
+
+}
