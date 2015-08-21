35275b69640f09b4f62f55a75b27dbbb1e16f767
anonymframework <serif.saglam119@gmail.com>
2015-08-21 08:30:12 +0300

    updated bind method

--- src/Anonym/Patterns/Singleton.php
+++ src/Anonym/Patterns/Singleton.php
@@ -31,10 +31,19 @@ class Singleton
      * @param string $class the name of class
      * @param callable $callback the callback for name
      * @throws InvalidArgumentException
+     * @return mixed
      */
     public static function bind($class, callable $callback)
     {
 
-
+        if(!is_string($class))
+        {
+            throw new InvalidArgumentException(sprintf('Class name must be a string'));
+        }
+        if(!isset(static::$binded[$class]))
+        {
+            static::$binded[$class] = $callback;
+        }
+        return call_user_func(static::$binded[$class]);
     }
 }
