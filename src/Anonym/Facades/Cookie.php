b352be2ab50ddf01d725b38f67caca32d281ede9
anonymframework <serif.saglam119@gmail.com>
2015-08-21 21:17:37 +0300

    added cookie facade

---
+++ src/Anonym/Facades/Cookie.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Facades;
+
+/**
+ * Class Cookie
+ * @package Anonym\Facades
+ */
+class Cookie
+{
+
+
+}
