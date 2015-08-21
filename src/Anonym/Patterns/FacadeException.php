33ac6982b1bb21e241e497abf147453441752bef
anonymframework <serif.saglam119@gmail.com>
2015-08-21 05:44:15 +0300

    updated facade exception

--- src/Anonym/Patterns/FacadeException.php
+++ src/Anonym/Patterns/FacadeException.php
@@ -10,8 +10,22 @@
 
 namespace Anonym\Patterns;
 
-
+/**
+ * the exception of facade
+ *
+ * Class FacadeException
+ * @package Anonym\Patterns
+ */
 class FacadeException
 {
 
+    /**
+     * throw the exception
+     *
+     * @param string $message the message of exception
+     */
+    public function __construct($message = '')
+    {
+        $this->message = $message;
+    }
 }
