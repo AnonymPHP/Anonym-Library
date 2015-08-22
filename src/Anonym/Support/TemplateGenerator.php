00ea4cbb153dbfe77db157d7efdda0a40ebcdf8f
anonymframework <serif.saglam119@gmail.com>
2015-08-22 13:17:01 +0300

    added setters

--- src/Anonym/Support/TemplateGenerator.php
+++ src/Anonym/Support/TemplateGenerator.php
@@ -17,4 +17,44 @@ namespace Anonym\Support;
 class TemplateGenerator
 {
 
+    /**
+     * the content of template
+     *
+     * @var string
+     */
+    private  $content;
+
+    public function generate($parameters = [])
+    {
+
+        $content = $this->getContent();
+        if (count($parameters)) {
+            foreach($parameters as $param)
+            {
+                 $this
+            }
+        }
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     * @return TemplateGenerator
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+
+
 }
