1859069f850a4b298786487ea1242aa35dfdfe6b
anonymframework <serif.saglam119@gmail.com>
2015-08-22 04:49:54 +0300

    added model facade

---
+++ src/Anonym/Facades/Model.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Facades;
+
+
+class Model
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
