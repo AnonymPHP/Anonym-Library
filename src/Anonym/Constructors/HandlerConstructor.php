040b80db9982b1a0a5fbf2d4522074627ae9d7a2
anonymframework <serif.saglam119@gmail.com>
2015-08-23 02:16:40 +0300

    added set error handler

--- src/Anonym/Constructors/HandlerConstructor.php
+++ src/Anonym/Constructors/HandlerConstructor.php
@@ -11,12 +11,27 @@
 namespace Anonym\Constructors;
 
 
+use Anonym\Support\ErrogBag;
+use Anonym\Support\ErrorJar;
+
 class HandlerConstructor
 {
 
+    /**
+     *  create a new instance and set the error handler
+     *
+     */
     public function __construct()
     {
 
+        // set the error handler
+        set_error_handler(function($code, $messsage, $file, $line){
+            ErrogBag::addError(new ErrorJar($messsage, $file, $line, $code));
+        });
+
+        set_exception_handler(function($exception){
+           ErrogBag::
+        });
     }
 
 }
