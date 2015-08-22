178272658e8494dedc99efa80980fdfb63654721
anonymframework <serif.saglam119@gmail.com>
2015-08-22 04:51:37 +0300

    added new lines

--- src/Anonym/Facades/Model.php
+++ src/Anonym/Facades/Model.php
@@ -14,8 +14,19 @@ namespace Anonym\Facades;
 class Model
 {
 
+
     public function __construct()
     {
+        $called = get_called_class();
+
+        // if isset table variable
+        if (array_key_exists('table', get_class_vars($called))) {
+
+            // use it
+            $called = get_class_vars($called)['table'];
+        }
+
+
 
     }
 
