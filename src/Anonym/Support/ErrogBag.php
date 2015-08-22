bab0ddedcd7d704471e8b92daa1a25df756ec96b
anonymframework <serif.saglam119@gmail.com>
2015-08-22 22:56:53 +0300

    added $errors and $exceptions

--- src/Anonym/Support/ErrogBag.php
+++ src/Anonym/Support/ErrogBag.php
@@ -10,8 +10,27 @@
 
 namespace Anonym\Support;
 
-
+/**
+ * Class ErrogBag
+ * @package Anonym\Support
+ */
 class ErrogBag
 {
+
+    /**
+     * the repository of php errors
+     *
+     * @var array
+     */
+    private static $errors;
+
+    /**
+     * the repository of php exceptions
+     *
+     * @var array
+     */
+    private static $exceptions;
+
+
 
 }
