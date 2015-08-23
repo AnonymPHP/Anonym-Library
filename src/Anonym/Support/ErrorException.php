ea32d64024ccae6fca399a8f51d6ddef7555b0e1
anonymframework <serif.saglam119@gmail.com>
2015-08-23 06:18:33 +0300

    made some updates

--- src/Anonym/Support/ErrorException.php
+++ src/Anonym/Support/ErrorException.php
@@ -10,14 +10,41 @@
 
 namespace Anonym\Support;
 
-use Exception;
-
 /**
  * Class ErrorException
  * @package Anonym\Support
  */
-class ErrorException extends Exception
+class ErrorException
 {
+
+    /**
+     * the code of error
+     *
+     * @var int
+     */
+    private $code;
+
+    /**
+     * the message of error
+     *
+     * @var string
+     */
+    private $message;
+
+    /**
+     * the file of error
+     *
+     * @var int
+     */
+    private $file;
+
+    /**
+     * the line of error
+     *
+     * @var int
+     */
+    private $line;
+
 
     /**
      * throw the exception
@@ -34,4 +61,78 @@ class ErrorException extends Exception
         $this->file = $file;
         $this->line = $line;
     }
+
+    /**
+     * @return int
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param int $code
+     * @return ErrorException
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return ErrorException
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param int $file
+     * @return ErrorException
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLine()
+    {
+        return $this->line;
+    }
+
+    /**
+     * @param int $line
+     * @return ErrorException
+     */
+    public function setLine($line)
+    {
+        $this->line = $line;
+        return $this;
+    }
+
+
 }
