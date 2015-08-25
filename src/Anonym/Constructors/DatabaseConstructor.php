f6b0cab1e296de48ef193b537707b77e19fd4ae6
anonymframework <serif.saglam119@gmail.com>
2015-08-25 12:02:36 +0300

    removed database provider

--- src/Anonym/Constructors/DatabaseConstructor.php
+++ src/Anonym/Constructors/DatabaseConstructor.php
@@ -11,7 +11,23 @@
 namespace Anonym\Constructors;
 
 
+use Anonym\Bootstrap\Container;
+use Anonym\Components\Database\Base;
+
 class DatabaseConstructor
 {
+    use Container;
 
+    public function __construct()
+    {
+        $this->singleton(
+            'database.base',
+            function () {
+                $configs = Config::get('database');
+                $connection = $configs['connection'];
+                $connectionConfigs = Arr::get($configs['connections'], $connection, []);
+                return new Base($connectionConfigs);
+            }
+        );
+    }
 }
