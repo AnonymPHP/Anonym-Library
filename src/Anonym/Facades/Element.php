c57aca7975d231a446504d8a9f2469d70f5b541c
anonymframework <serif.saglam119@gmail.com>
2015-08-25 14:57:41 +0300

    updated element facade class

--- src/Anonym/Facades/Element.php
+++ src/Anonym/Facades/Element.php
@@ -9,24 +9,85 @@
  */
 
 namespace Anonym\Facades;
-use Anonym\Patterns\Facade;
-use Anonym\Components\Element\Element as ElementDispatcher;
+
+use Anonym\Components\Element\Element as ElementOrm;
 use Anonym\Patterns\Singleton;
 
 /**
  * Class Element
- * @package Anonym\Facades
+ * @package Anonym\Components\Facade
  */
-class Element extends Facade
+class Element
 {
+
+    private static $table = 'table';
+
     /**
-     * get the element facade
-     *
-     * @return Element
+     * @var ElementOrm
      */
-    protected static function getFacadeClass()
+    public $orm;
+
+    /**
+     * Sınıfı başlatır
+     */
+    public function __construct()
     {
-        $base = Singleton::bind('database.base');
-        return new ElementDispatcher($base);
+        $this->orm = new ElementOrm(Singleton::bind('database.base'));
+        $this->orm->setTable($this->findCalledClassTableVariable());
+    }
+
+    /**
+     * Hangi sınıftan çağrıldığını arar
+     *
+     * @return mixed
+     */
+    private function findCalledClassTableVariable()
+    {
+        $class = get_called_class();
+        $vars = get_class_vars($class);
+        if (isset($vars[self::$table])) {
+            return $vars[self::$table];
+        } else {
+            return $this->resolveClassName($class);
+        }
+    }
+
+
+    /**
+     * resolve the class name
+     *
+     * @param string $name
+     * @return mixed
+     */
+    private function resolveClassName($name = '')
+    {
+        $explodeClass = explode('\\', $name);
+        return end($explodeClass);
+    }
+
+    /**
+     * Static kullanım desteği
+     *
+     * @param       $method
+     * @param array $params
+     * @return mixed
+     */
+    public static function __callStatic($method, $params = [])
+    {
+        $instance = new static();
+
+        return call_user_func_array([$instance->orm, $method], $params);
+    }
+
+    /**
+     * call the methods
+     *
+     * @param       $method
+     * @param array $params
+     * @return mixed
+     */
+    public function __call($method, $params = [])
+    {
+        return call_user_func_array([$this->orm, $method], $params);
     }
 }
