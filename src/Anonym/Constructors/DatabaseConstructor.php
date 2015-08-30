169cdc51cf03a8d93f69d107c7e42f9a5fea1842
anonymframework <serif.saglam119@outlook.com>
2015-08-30 18:58:43 +0300

    made some updates

--- src/Anonym/Constructors/DatabaseConstructor.php
+++ src/Anonym/Constructors/DatabaseConstructor.php
@@ -11,6 +11,7 @@
 namespace Anonym\Constructors;
 
 
+use Anonym\Bootstrap\Bootstrap;
 use Anonym\Bootstrap\Container;
 use Anonym\Components\Database\Base;
 use Anonym\Facades\Config;
@@ -21,20 +22,23 @@ use Anonym\Support\Arr;
  * Class DatabaseConstructor
  * @package Anonym\Constructors
  */
-class DatabaseConstructor extends Container
+class DatabaseConstructor
 {
 
     /**
      *  register the database base
+     *
+     * @param Boostrap $app
      */
-    public function __construct()
+    public function __construct(Bootstrap $app)
     {
-        $this->singleton(
+        $app->singleton(
             'database.base',
             function () {
                 $configs = Config::get('database');
                 $connection = $configs['connection'];
                 $connectionConfigs = Arr::get($configs['connections'], $connection, []);
+
                 return new Base($connectionConfigs);
             }
         );
