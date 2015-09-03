67665691effe08d26b4ad7b9982424048b0f5547
anonymframework <serif.saglam119@outlook.com>
2015-09-03 15:38:21 +0300

    fixed a little bug

--- src/Anonym/Providers/RouteProvider.php
+++ src/Anonym/Providers/RouteProvider.php
@@ -13,6 +13,7 @@ namespace Anonym\Providers;
 
 use Anonym\Bootstrap\ServiceProvider;
 use Anonym\Components\Route\Router;
+use Anonym\Facades\Config;
 
 class RouteProvider extends ServiceProvider
 {
@@ -25,7 +26,7 @@ class RouteProvider extends ServiceProvider
     public function register()
     {
         // if php on server, run router
-        if (function_exists('getallheaders')) {
+        if (Config::get('general.server' === 1 )) {
             include ROUTE_PHP;
             $router = new Router($this->make('http.request'));
             $router->run();
