96bf588c1acf3ca2e044648ecc3e0fe5ad6c7d18
anonymframework <serif.saglam119@outlook.com>
2015-09-12 18:56:09 +0300

    added blade

--- index.php
+++ index.php
@@ -9,11 +9,3 @@
  */
 
 include 'vendor/autoload.php';
-ini_set('display_errors', 'On');
-use Philo\Blade\Blade;
-
-$views = __DIR__ . '/views';
-$cache = __DIR__ . '/cache';
-
-$blade = new Blade($views, $cache);
-echo $blade->view()->make('hello')->render();
