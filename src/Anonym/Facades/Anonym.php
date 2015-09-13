5bf0f7c00b01b308a2ef61ed97587257164dbafa
anonymframework <serif.saglam119@outlook.com>
2015-09-13 20:53:33 +0300

    made some updates

--- src/Anonym/Facades/Anonym.php
+++ src/Anonym/Facades/Anonym.php
@@ -24,6 +24,6 @@ class Anonym extends Facade
      * @return string
      */
     protected static function getFacadeClass(){
-        return App::make(Kernel::class, [App::make('app')]);
+        return App::make('console');
     }
 }
