9cac130ebc110d2168bfa38b3302479bc5c62d06
anonymframework <serif.saglam119@outlook.com>
2015-09-13 20:09:46 +0300

    made some updates

--- src/Anonym/Http/FormRequest.php
+++ src/Anonym/Http/FormRequest.php
@@ -10,11 +10,22 @@
 
 namespace Anonym\Http;
 use Anonym\Components\HttpClient\Request;
+use Anonym\Facades\App;
+
 /**
  * Class FormRequest
  * @package Anonym\Http
  */
 class FormRequest extends Request
 {
+
+    /**
+     * get the response for a forbidden operation
+     *
+     * @return mixed
+     */
+    public function forbiddenResponse(){
+        return App::make('http.response')->setStatusCode(403)->setContent('Forbidden');
+    }
 
 }
