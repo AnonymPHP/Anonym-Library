4f36fdac98ba55fd11e82f630b723175942bad5e
anonymframework <serif.saglam119@outlook.com>
2015-09-14 20:05:32 +0300

    made some updates

--- src/Anonym/Support/ErrorBag.php
+++ src/Anonym/Support/ErrorBag.php
@@ -14,4 +14,52 @@ namespace Anonym\Support;
 class ErrorBag
 {
 
+    /**
+     * an array type of errors
+     *
+     * @var array
+     */
+    protected $errors = [];
+
+    /**
+     * add an error
+     *
+     * @param string $message
+     */
+    public function add($message = ''){
+        $this->errors[] = $message;
+
+        return $this;
+    }
+
+    /**
+     * get an  error
+     *
+     * @param int $index
+     * @return string
+     */
+    public function get($index){
+        return isset($this->errors[$index]) ? $this->errors[$index] : '';
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @param array $errors
+     * @return ErrorBag
+     */
+    public function setErrors(array $errors = [])
+    {
+        $this->errors = $errors;
+        return $this;
+    }
+
+
+
 }
