f2c0c4b765ae980cb166f8e60fdeb064380a21d8
anonymframework <serif.saglam119@outlook.com>
2015-09-14 20:06:50 +0300

    made some updates

--- src/Anonym/Providers/ErrorBagServiceProvide.php
+++ src/Anonym/Providers/ErrorBagServiceProvide.php
@@ -11,9 +11,22 @@
 namespace Anonym\Providers;
 
 
+use Anonym\Facades\App;
+use Anonym\Support\ErrorBag;
 use Illuminate\Support\ServiceProvider;
 
 class ErrorBagServiceProvider extends ServiceProvider
 {
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::singleton('errors.bag', function(){
+           return new ErrorBag();
+        });
+    }
 }
