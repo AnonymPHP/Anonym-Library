b980530ea0cb215c5d0923d03e6bdfdec4e785df
anonymframework <serif.saglam119@outlook.com>
2015-09-20 00:13:30 +0300

    added alias loader

---
+++ src/Anonym/Application/AliasLoader.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Application;
+
+/**
+ * Class AliasLoader
+ * @package Anonym\Application
+ */
+class AliasLoader
+{
+
+    /**
+     * the type of array for aliases
+     *
+     * @var array
+     */
+    private static $aliases;
+
+
+    /**
+     * load a class with it's alias
+     *
+     * @param string $alias
+     * @return bool
+     */
+    public function load($alias){
+
+        if (isset(static::$aliases[$alias])) {
+            return class_alias(static::$aliases[$alias], $alias);
+        }
+
+    }
+
+    /**
+     * Add an alias to the loader.
+     *
+     * @param  string  $class
+     * @param  string  $alias
+     * @return void
+     */
+    public function alias($class, $alias)
+    {
+        static::$aliases[$class] = $alias;
+    }
+
+}
