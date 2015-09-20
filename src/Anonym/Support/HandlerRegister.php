d876294deafaad68f9906ec7b59887dda9de547a
anonymframework <serif.saglam119@outlook.com>
2015-09-20 07:29:53 +0300

    made some updates

---
+++ src/Anonym/Support/HandlerRegister.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Support;
+
+/**
+ * Class HandlerRegister
+ * @package Anonym\Support
+ */
+class HandlerRegister
+{
+
+    /**
+     *  register the error handler
+     */
+    public static function register(){
+
+    }
+}
