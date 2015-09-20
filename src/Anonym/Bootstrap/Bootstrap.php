f3b8ffa084ca8c4ea36505727a40b3aad95176a1
anonymframework <serif.saglam119@outlook.com>
2015-09-20 16:06:49 +0300

    made some updates

--- src/Anonym/Bootstrap/Bootstrap.php
+++ src/Anonym/Bootstrap/Bootstrap.php
@@ -191,7 +191,7 @@ class Bootstrap extends Container
         $bootstraps = $this->constructors;
         foreach ($bootstraps as $boot) {
             if (is_string($boot)) {
-                $this->singleton($boot, new $boot($this));
+                $this->make($boot, [$this]);
             }
         }
     }
