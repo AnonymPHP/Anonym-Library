b4ca60d7660391afa52dc8965378ad547b7df484
anonymframework <serif.saglam119@outlook.com>
2015-09-20 15:52:50 +0300

    made some updates

--- src/Anonym/Support/Handler.php
+++ src/Anonym/Support/Handler.php
@@ -9,9 +9,42 @@
  */
 
 namespace Anonym\Support;
+use Exception;
+use HttpException;
+use ErrorException;
 
 
+/**
+ * Class Handler
+ * @package Anonym\Support
+ */
 class Handler
 {
 
+    /**
+     * convert to errors to exceptions
+     *
+     * @param int $code
+     * @param string $message
+     * @param string $file
+     * @param int $line
+     * @throws ErrorException
+     */
+    public function handleErrors($code, $message, $file, $line){
+
+        // throw a new error.
+        throw new ErrorException($message, 0, $code, $file, $line);
+    }
+
+    /**
+     * @param Exception $e
+     * @return mixed
+     */
+    public function handleExceptions(Exception $e){
+
+    }
+
+    protected function isHttpException(Exception $e){
+        return $e instanceof HttpException;
+    }
 }
