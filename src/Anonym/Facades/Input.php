fb56e84fb36443670cd0a7c48e441140ff67c430
anonymframework <serif.saglam119@outlook.com>
2015-09-20 04:05:42 +0300

    made some updates

---
+++ src/Anonym/Facades/Input.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Facades;
+
+
+use Anonym\Patterns\Facade;
+use Anonym\Components\HttpClient\Input as Abst;
+
+/**
+ * Class Input
+ * @package Anonym\Facades
+ */
+class Input extends Facade
+{
+
+    /**
+     * get the query facade
+     *
+     * @return string
+     */
+    protected static function getFacadeClass(){
+        return Abst::class;
+    }
+}
