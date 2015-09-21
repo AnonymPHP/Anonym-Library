7853ff8bab93819aaee506c4f4f28ec2db727ce5
anonymframework <serif.saglam119@outlook.com>
2015-09-21 04:37:24 +0300

    made some updates

--- src/Anonym/Facades/Element.php
+++ src/Anonym/Facades/Element.php
@@ -41,7 +41,7 @@ class Element
      */
     private function findCalledClassTableVariable()
     {
-        $vars = get_class_vars($this);
+        $vars = get_class_vars(self::class);
 
         if (isset($vars[ElementOrm::TABLE_NAME])) {
             return $vars[ElementOrm::TABLE_NAME];
@@ -84,7 +84,8 @@ class Element
      * @param string $table
      * @return $this
      */
-    public static function table($table){
+    public static function table($table)
+    {
         $instance = new static();
 
         $instance->orm->setTable($table);
