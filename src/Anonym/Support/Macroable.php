2562666993689fc22402c276469fa15667b0ab6c
anonymframework <serif.saglam119@outlook.com>
2015-09-23 04:13:56 +0300

    made some updates

--- src/Anonym/Support/Macroable.php
+++ src/Anonym/Support/Macroable.php
@@ -39,7 +39,11 @@ class Macroable
         return $this;
     }
 
-
+    /**
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
     public function __call($method, $args){
         if (isset($this->macros[$method])) {
             return call_user_func_array($this->macros[$method], $args);
