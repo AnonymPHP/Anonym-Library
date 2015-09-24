e3c78bc59fdd6d76cef85a5e693e5b8d090bca8a
anonymframework <serif.saglam119@outlook.com>
2015-09-24 11:14:02 +0300

    made some updates

--- src/Anonym/Database/QueryBuilder/Sql/Insert/SingleInsert.php
+++ src/Anonym/Database/QueryBuilder/Sql/Insert/SingleInsert.php
@@ -10,8 +10,27 @@
 
 namespace Anonym\Database\QueryBuilder\Sql;
 
-
+/**
+ * Class SingleInsert
+ * @package Anonym\Database\QueryBuilder\Sql
+ */
 class SingleInsert
 {
+
+    /**
+     * the pattern string
+     *
+     * @var string
+     */
+    protected $pattern;
+
+    /**
+     * create a new instance and register pattern
+     *
+     * @param array $patterns
+     */
+    public function __construct($patterns = []){
+        $this->pattern = $patterns['single'];
+    }
 
 }
