b4ccd264d5857171ccbafa5399aabf8a17bcc820
anonymframework <serif.saglam119@outlook.com>
2015-09-24 11:04:56 +0300

    made some updates

---
+++ src/Anonym/Database/QueryBuilder/Sql/Builder.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Database\QueryBuilder\Sql;
+
+/**
+ * Class Builder
+ * @package Anonym\Database\QueryBuilder
+ */
+class Builder extends QueryPatterns
+{
+
+    /**
+     *  the constants for selected pattern
+     */
+    const WITH_WHERE = 1;
+    const WITHOUT_WHERE = 0;
+
+    /**
+     * the mode of read
+     *
+     * @var int
+     */
+    protected $mode;
+
+}
