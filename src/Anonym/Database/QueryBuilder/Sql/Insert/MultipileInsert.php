2700b86d9476704d4328b78e1b412c477a03bc86
anonymframework <serif.saglam119@outlook.com>
2015-09-24 14:33:27 +0300

    made some updates

--- src/Anonym/Database/QueryBuilder/Sql/Insert/MultipileInsert.php
+++ src/Anonym/Database/QueryBuilder/Sql/Insert/MultipileInsert.php
@@ -11,7 +11,40 @@
 namespace Anonym\Database\QueryBuilder\Sql\Insert;
 
 
-class MultipileInsert
+use Anonym\Database\QueryBuilder\QueryBuilder;
+
+/**
+ * Class MultipileInsert
+ * @package Anonym\Database\QueryBuilder\Sql\Insert
+ */
+class MultipileInsert extends QueryBuilder
 {
 
+    /**
+     * @var array
+     */
+    protected $parameters;
+
+    /**
+     * create a new instance
+     *
+     * @param array $patterns
+     * @param array $parameters
+     * @param $table
+     */
+    public function __construct($patterns, $parameters, $table){
+        $this->pattern = $patterns['multipile'];
+        $this->parameters = $parameters;
+        $this->table = $table;
+    }
+
+    /**
+     * build and return query string
+     *
+     * @return string
+     */
+    public function buildQuery()
+    {
+
+    }
 }
