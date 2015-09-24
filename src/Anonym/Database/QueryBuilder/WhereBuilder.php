b2bc7282ee839b1bb01ff2b6d506178687011e09
anonymframework <serif.saglam119@outlook.com>
2015-09-25 00:05:23 +0300

    made some updates

--- src/Anonym/Database/QueryBuilder/WhereBuilder.php
+++ src/Anonym/Database/QueryBuilder/WhereBuilder.php
@@ -19,29 +19,5 @@ use Anonym\Support\Str;
 class WhereBuilder
 {
 
-    public function buildWhereQuery($where = [])
-    {
-
-        $builded = '';
-
-        if (is_object($where)) {
-            $where = (array)$where;
-        }
-
-        foreach ($where as $w) {
-
-            list($column, $operator, $value, $mode) = $w;
-            $builded .= "$column $operator $value $mode";
-        }
-
-
-        if (Str::endsWith($builded, 'AND')) {
-            $builded = rtrim($builded, 'AND');
-        } elseif (Str::endsWith($builded, 'OR')) {
-            $builded = rtrim($builded, 'OR');
-        }
-
-        return $builded;
-    }
 
 }
