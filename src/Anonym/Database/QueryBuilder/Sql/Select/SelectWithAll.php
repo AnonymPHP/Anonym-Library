2454dd50854957f261147e5a93f3cebb64e2a9c3
anonymframework <serif.saglam119@outlook.com>
2015-09-25 01:44:59 +0300

    made some updates

--- src/Anonym/Database/QueryBuilder/Sql/Select/SelectWithAll.php
+++ src/Anonym/Database/QueryBuilder/Sql/Select/SelectWithAll.php
@@ -10,8 +10,33 @@
 
 namespace Anonym\Database\QueryBuilder\Sql\Select;
 
-
-class SelectWithAll
+use Anonym\Database\QueryBuilder\QueryBuilder;
+/**
+ * Class SelectWithAll
+ * @package Anonym\Database\QueryBuilder\Sql\Select
+ */
+class SelectWithAll extends QueryBuilder
 {
 
+    /**
+     * create a new instance
+     *
+     * @param array $patterns
+     * @param array $parameters
+     * @param $table
+     */
+    public function __construct($patterns, $parameters, $table){
+        $this->prepareToBuild($patterns);
+        $this->parameters = $parameters;
+        $this->table = $table;
+    }
+    /**
+     * build and return query string
+     *
+     * @return string
+     */
+    public function buildQuery()
+    {
+
+    }
 }
