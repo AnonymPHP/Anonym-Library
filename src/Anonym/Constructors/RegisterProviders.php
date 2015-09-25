62000f7ce03fe5a4e3548a7046545523046be386
anonymframework <serif.saglam119@outlook.com>
2015-09-25 14:58:07 +0300

    made some updates

---
+++ src/Anonym/Constructors/RegisterProviders.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace Anonym\Bootstrap;
+use Anonym\Facades\Config;
+/**
+ * Class RegisterProviders
+ * @package Anonym\Bootstrap
+ */
+class RegisterProviders
+{
+
+    /**
+     * create a new instance
+     *
+     * @param Bootstrap $app
+     * @throws BindNotRespondingException
+     * @throws ProviderException
+     */
+    public function __construct(Bootstrap $app)
+    {
+
+        $providers = Config::get('general.providers');
+
+        foreach ($providers as $provider) {
+            $provider = new $provider();
+
+            if (!$provider instanceof ServiceProvider) {
+                throw new ProviderException(sprintf('Your %s proiver must be a instance of ServiceProvider', get_class($provider)));
+            }
+
+            $provider->register();
+        }
+    }
+
+}
