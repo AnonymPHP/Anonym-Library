40a7c6b8a0a3bd5bb0f03d5cb7ca839dc89ea68e
anonymframework <serif.saglam119@outlook.com>
2015-09-25 03:07:32 +0300

    made some updates

--- index.php
+++ index.php
@@ -15,6 +15,6 @@ $builder = new Anonym\Database\QueryBuilder\Sql\Builder($container);
 
 $builder->table('users')->orWhere('key', 'value')->where('aa', 'bb')->group('test')->orderBy('id', 'DESC');
 
-
+$builder->join(new \Anonym\Database\QueryBuilder\Sql\Join('INNER', 'user_id', 'kategoriler', 'kategori_id'))
 
 print_r($builder->get());
