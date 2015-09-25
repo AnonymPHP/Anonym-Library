7d73d6087bdcd98d6a186dcea6a1299267cadb97
anonymframework <serif.saglam119@outlook.com>
2015-09-25 06:30:36 +0300

    made some updates

--- src/Anonym/Element/Query/Grammer/Grammer.php
+++ src/Anonym/Element/Query/Grammer/Grammer.php
@@ -14,4 +14,30 @@ namespace Anonym\Element\Query\Grammer;
 abstract class Grammer
 {
 
+    /**
+     * compile all insert values with special grammer
+     *
+     * @param array $values
+     * @return mixed
+     */
+    abstract public function compileInsertValues(array $values = []);
+
+
+    /**
+     * compile all update values with special grammer
+     *
+     * @param array $values
+     * @return mixed
+     */
+    abstract public function compileUpdateValues(array $values = []);
+
+
+    /**
+     * compile all where values with special grammer
+     *
+     * @param array $values
+     * @return mixed
+     */
+    abstract public function compileWhereValues(array $values = []);
+
 }
