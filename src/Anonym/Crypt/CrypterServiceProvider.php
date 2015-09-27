65e26959e8e1917c556ec0bff174768ef275e07c
anonymframework <serif.saglam119@outlook.com>
2015-09-27 03:49:56 +0300

    made some updates

---
+++ src/Anonym/Crypt/CrypterServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Crypt;
+use Anonym\Application\ServiceProvider;
+/**
+ * Class CrypterServiceProvider
+ * @package Anonym\Crypt
+ */
+class CrypterServiceProvider extends ServiceProvider
+{
+
+    /**
+     * register the provider
+     *
+     * @return mixed
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
