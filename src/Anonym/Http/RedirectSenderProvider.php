5dd47744ceaef1bafc0aa7445728a4c501b52285
anonymframework <serif.saglam119@outlook.com>
2015-09-28 22:11:57 +0300

    made some updates

---
+++ src/Anonym/Http/RedirectSenderProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Http;
+
+
+use Anonym\Application\ServiceProvider;
+
+/**
+ * Class RedirectSenderProvider
+ * @package Anonym\Http
+ */
+class RedirectSenderProvider extends ServiceProvider
+{
+
+}
