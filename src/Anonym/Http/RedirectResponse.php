fabac0fc48de948e3f33a42335d118021d7dd20e
anonymframework <serif.saglam119@outlook.com>
2015-09-30 01:51:54 +0300

    made some updates

--- src/Anonym/Http/RedirectResponse.php
+++ src/Anonym/Http/RedirectResponse.php
@@ -1,129 +1,129 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-    namespace Anonym\Http;
-    use Anonym\Http\RedirectUrlEmptyException;
+namespace Anonym\Http;
+use Anonym\Http\RedirectUrlEmptyException;
+
+/**
+ * Class Redirect
+ * @package Anonym\Http
+ */
+class RedirectResponse extends Response implements ResponseInterface
+{
 
     /**
-     * Class Redirect
-     * @package Anonym\Http
+     * Yönlendirme için beklenecek zaman
+     *
+     * @var integer
      */
-    class RedirectResponse extends Response implements ResponseInterface
+    private $time;
+    /**
+     * Yönlendirilecek url i atar
+     *
+     * @var string
+     */
+    private $target;
+
+    /**
+     * Yönlendirme işlemi yapar
+     *
+     * @param string  $adress
+     * @param integer $time
+     * @throws RedirectUrlEmptyException
+     */
+    public function __construct($adress = '', $time = 0)
     {
-
-        /**
-         * Yönlendirme için beklenecek zaman
-         *
-         * @var integer
-         */
-        private $time;
-        /**
-         * Yönlendirilecek url i atar
-         *
-         * @var string
-         */
-        private $target;
-
-        /**
-         * Yönlendirme işlemi yapar
-         *
-         * @param string  $adress
-         * @param integer $time
-         * @throws RedirectUrlEmptyException
-         */
-        public function __construct($adress = '', $time = 0)
-        {
-
-            if($adress === ''){
-                throw new RedirectUrlEmptyException('Yönlendirilecek url boş olamaz');
-            }
-
-            parent::__construct();
-            $this->setTarget($adress);
-            $this->setTime($time);
-        }
-        /**
-         *  Eski sayfaya geri döndürür
-         */
-        public function back()
-        {
-            self::to($_SERVER['HTTP_REFERER']);
-        }
-
-        /**
-         * @return string
-         */
-        public function getTarget()
-        {
-            return $this->target;
-        }
-
-        /**
-         * @param string $target
-         * @return Redirect
-         */
-        public function setTarget($target)
-        {
-            $this->target = $target;
-
-            return $this;
-        }
-
-        /**
-         * @return int
-         */
-        public function getTime()
-        {
-            return $this->time;
-        }
-
-        /**
-         * @param int $time
-         * @return Redirect
-         */
-        public function setTime($time)
-        {
-            $this->time = $time;
-
-            return $this;
-        }
-
-
-        /**
-         * Yönlendirme işlemini yapar
-         *
-         * @throws HttpResponseException
-         */
-        public function send(){
-
-            $time = $this->getTime();
-            $target = $this->getTarget();
-            if($time === 0)
-            {
-                $this->header("Location", $target);
-            }else{
-                $this->header(sprintf('Refresh:%d, url=%s', $time, $target));
-            }
-
-            $this->send();
-        }
-
-
-        /**
-         * Static olarak içeriği oluşturur
-         *
-         * @param string $target
-         * @param int $time
-         * @return static
-         */
-        public static function create($target = '', $time = 0)
-        {
-            return new static($target, $time);
-        }
+        parent::__construct();
+        $this->setTarget($adress);
+        $this->setTime($time);
     }
+    /**
+     *  Eski sayfaya geri döndürür
+     */
+    public function back()
+    {
+        self::to($_SERVER['HTTP_REFERER']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * @param string $target
+     * @return Redirect
+     */
+    public function setTarget($target)
+    {
+        $this->target = $target;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param int $time
+     * @return Redirect
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+
+    /**
+     * Yönlendirme işlemini yapar
+     *
+     * @throws HttpResponseException
+     */
+    public function send(){
+
+        $time = $this->getTime();
+        $target = $this->getTarget();
+
+
+        if($target === ''){
+            throw new RedirectUrlEmptyException('Yönlendirilecek url boş olamaz');
+        }
+
+        if($time === 0)
+        {
+            $this->header("Location", $target);
+        }else{
+            $this->header(sprintf('Refresh:%d, url=%s', $time, $target));
+        }
+        $this->send();
+    }
+
+
+    /**
+     * Static olarak içeriği oluşturur
+     *
+     * @param string $target
+     * @param int $time
+     * @return static
+     */
+    public static function create($target = '', $time = 0)
+    {
+        return new static($target, $time);
+    }
+}
