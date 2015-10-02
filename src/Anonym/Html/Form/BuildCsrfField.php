29400f0ca2b55a30ed7939465fa2955e6954945a
anonymframework <serif.saglam119@outlook.com>
2015-10-02 22:08:35 +0300

    made some updates

---
+++ src/Anonym/Html/Form/BuildCsrfField.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Html\Form;
+
+/**
+ * Class BuildCsrfField
+ * @package Anonym\Html\Form
+ */
+trait BuildCsrfField
+{
+
+}
