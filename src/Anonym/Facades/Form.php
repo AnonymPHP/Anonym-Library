9c8e60726c6871dabe4b7fcf25132b9496d722fd
anonymframework <serif.saglam119@outlook.com>
2015-10-03 22:34:42 +0300

    made some updates

---
+++ src/Anonym/Facades/Form.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Facades;
+
+
+use Anonym\Patterns\Facade;
+
+class Form extends Facade
+{
+
+}
