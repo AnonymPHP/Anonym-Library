6004ecd010bf4afb24dcced496e0b21f07e3e83e
anonymframework <serif.saglam119@gmail.com>
2015-10-06 21:44:50 +0300

    made some updates

--- src/Anonym/Application/Console/MakeBlade.php
+++ src/Anonym/Application/Console/MakeBlade.php
@@ -14,8 +14,6 @@ namespace Anonym\Application\Console;
 use Anonym\Console\Command;
 use Anonym\Console\HandleInterface;
 use Anonym\Facades\Anonym;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputInterface;
 
 /**
  * Class MakeBlade
@@ -38,8 +36,6 @@ class MakeBlade extends Command implements HandleInterface
 
     /**
      *
-     * @param InputInterface $input
-     * @param OutputInterface $output
      * @return mixed
      */
     public function handle()
