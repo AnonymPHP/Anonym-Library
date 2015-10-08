8baddb508a402479bb0b711f21f2e7a3644915b4
anonymframework <serif.saglam119@gmail.com>
2015-10-08 19:26:32 +0300

    created abstract cipher class

---
+++ src/Anonym/Crypt/Cipher.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Crypt;
+
+/**
+ * Class Cipher
+ * @package Anonym\Crypt
+ */
+abstract class Cipher
+{
+
+}
