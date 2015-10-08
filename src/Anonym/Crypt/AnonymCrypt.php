c4358a9ceb5e59f86beb2fef3076fb2f902d8b90
anonymframework <serif.saglam119@gmail.com>
2015-10-08 11:46:30 +0300

    made some updates

--- src/Anonym/Crypt/AnonymCrypt.php
+++ src/Anonym/Crypt/AnonymCrypt.php
@@ -1,45 +1,45 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-    namespace Anonym\Crypt;
-    use Exception;
-    use InvalidArgumentException;
+namespace Anonym\Crypt;
+
+use Exception;
+
+/**
+ * Class AnonymCrypt
+ * @package Anonym\Crypt
+ */
+class AnonymCrypt implements CryptInterface
+{
+
     /**
-     * Class AnonymCrypt
-     * @package Anonym\Crypt
+     * the random key for security key
+     *
+     * @var string
      */
-    class AnonymCrypt implements CryptInterface
+    protected $applicationKey;
+
+
+    /**
+     * Sınıfı başlatır
+     *
+     * @throws Exception
+     */
+    public function __construct($applicationKey)
     {
-
-        /**
-         * the random key for security key
-         *
-         * @var string
-         */
-        protected  $applicationKey;
-
-
-        /**
-         * Sınıfı başlatır
-         *
-         * @throws Exception
-         */
-        public function __construct($applicationKey)
-        {
-            if (!function_exists('openssl_encrypt')) {
-                throw new Exception('OpenSsl extension is must be installed on your server.');
-            }
-
-
-            $this->applicationKey = $applicationKey;
+        if (!function_exists('openssl_encrypt')) {
+            throw new Exception('OpenSsl extension is must be installed on your server.');
         }
 
 
-
+        $this->applicationKey = $applicationKey;
     }
+
+
+}
