0527c4d86b961e28427bed21ff56ca27daa3548f
anonymframework <serif.saglam119@gmail.com>
2015-10-08 12:01:46 +0300

    made some updates

--- src/Anonym/Crypt/SecurityKeyGenerator.php
+++ src/Anonym/Crypt/SecurityKeyGenerator.php
@@ -1,47 +1,49 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-    namespace Anonym\Crypt;
-    use Anonym\Facades\Config;
+namespace Anonym\Crypt;
+
+use Anonym\Facades\Config;
+
+/**
+ * Class SecurityKeyGenerator
+ * @package Anonym\Crypt
+ */
+class SecurityKeyGenerator
+{
 
     /**
-     * Class SecurityKeyGenerator
-     * @package Anonym\Crypt
+     * Özel güvenlik anahtarını oluşturur
+     *
+     * @param string $key
+     * @return string
      */
-    class SecurityKeyGenerator
+    public function create($key = null)
     {
 
-        /**
-         * Özel güvenlik anahtarını oluşturur
-         *
-         * @param string $key
-         * @return string
-         */
-        public function create($key = null)
-        {
-
-            if(!is_string($key)){
-                $key = 'AnonymFrameworkRandom.'.$_SERVER['SERVER_ADDR'];
-            }
-
-            return md5($key);
+        if (!is_string($key)) {
+            $key = 'AnonymFrameworkRandom.' . $_SERVER['SERVER_ADDR'];
         }
 
-        /**
-         * create a random string
-         *
-         * @param string $prefix
-         * @return string
-         */
-        public function random($prefix = ''){
-            $key = $prefix.$this->create().strval(rand(strlen($_SERVER['REMOTE_ADDR']), rand(111,2222)));
-            $uniqid = uniqid($key);
-            return md5($uniqid);
-        }
+        return md5($key);
     }
+
+    /**
+     * create a random string
+     *
+     * @param string $prefix
+     * @return string
+     */
+    public function random($prefix = '')
+    {
+        $key = $prefix . $this->create() . strval(rand(strlen($_SERVER['REMOTE_ADDR']), rand(111, 2222)));
+        $uniqid = uniqid($key);
+        return md5($uniqid);
+    }
+}
