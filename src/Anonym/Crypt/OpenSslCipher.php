a5bb53b76f0da62d1824bc178a1705278d9174bb
anonymframework <serif.saglam119@gmail.com>
2015-10-10 01:28:58 +0300

    made some updates

--- src/Anonym/Crypt/OpenSslCipher.php
+++ src/Anonym/Crypt/OpenSslCipher.php
@@ -15,6 +15,29 @@ class OpenSslCipher extends Cipher
 {
 
     /**
+     * the application key for security key
+     *
+     * @var string
+     */
+    protected $appKey;
+
+    /**
+     * the encrypting method for open ssl cipher
+     *
+     * @var string
+     */
+    protected $mode;
+
+    /**
+     * create a new instance and register the application key
+     *
+     * @param string $key
+     */
+    public function __construct($key){
+        $this->appKey = $key;
+    }
+
+    /**
      * encrypt the value
      *
      * @param string $value
