81ba6e1f7ffa11ea5f4ae0fe8f2cd91e6db04261
anonymframework <serif.saglam119@gmail.com>
2015-10-10 00:33:39 +0300

    made some updates

--- src/Anonym/Crypt/OpenSslCiphe.php
+++ src/Anonym/Crypt/OpenSslCiphe.php
@@ -30,7 +30,9 @@ class OpenSslCipher extends Cipher
 
 
     /**
-     * @param $key
+     * create a new instance and register security key
+     *
+     * @param string $key
      */
     public function __construct($key){
         $this->key = $key;
