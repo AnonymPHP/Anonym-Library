f42bee4e11227e333ed46d329c84bc9c70ca1f66
anonymframework <anonymframework@gemframework.com>
2015-11-15 18:01:00 +0200

    made some changes

---
+++ src/Anonym/Database/Tongue/Tongue.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  This file belongs to AnonymPHP Framework
+ *
+ * @author vahitserifsaglam1 <vahit.serif119@gmail.com>
+ * @website http://anonymphp.com/framework
+ */
+
+namespace Anonym\Database\Tongue;
+
+
+/**
+ * Class Tongue
+ * @package Anonym\Database\Tongue
+ */
+abstract class Tongue
+{
+
+}
