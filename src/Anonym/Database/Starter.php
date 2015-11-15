852b2c9345506224b9cba441376f08242b358651
anonymframework <anonymframework@gemframework.com>
2015-11-15 12:43:24 +0200

    made some changes

--- src/Anonym/Database/Starter.php
+++ src/Anonym/Database/Starter.php
@@ -97,5 +97,21 @@ class Starter
         return $this->db;
     }
 
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
 }
