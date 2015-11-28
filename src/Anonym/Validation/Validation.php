3a2a1b07b91d8d0bc1f8f62f56faaf598b0b6490
anonymframework <serif.saglam119@outlook.com>
2015-11-28 18:53:16 +0200

    made some updates

---
+++ src/Anonym/Validation/Validation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Validation;
+
+/**
+ * Class Validation
+ * @package Anonym\Validation
+ */
+class Validation
+{
+
+}
