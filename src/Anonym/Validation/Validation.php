2910bb6eaa680d9122c3c37d3d34d61eef2df285
anonymframework <serif.saglam119@outlook.com>
2015-12-05 15:58:50 +0200

    made some updates

--- src/Anonym/Validation/Validation.php
+++ src/Anonym/Validation/Validation.php
@@ -39,4 +39,72 @@ class Validation
      */
     protected $messageReposity;
 
+    /**
+     * Validation constructor.
+     *
+     * @param array $datas
+     * @param array $rules
+     */
+    public function __construct($datas = [], $rules = [], $messages = [])
+    {
+        $this->setDatas($datas)
+            ->setRules($rules)
+            ->setMessageReposity(new ValidationErrorMessage())
+            ->getMessageReposity()->setErrors($messages);
+    }
+
+    /**
+     * @return array
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param array $rules
+     * @return Validation
+     */
+    public function setRules($rules)
+    {
+        $this->rules = $rules;
+        return $this;
+    }
+
+    /**
+     * @return ValidationErrorMessage
+     */
+    public function getMessageReposity()
+    {
+        return $this->messageReposity;
+    }
+
+    /**
+     * @param ValidationErrorMessage $messageReposity
+     * @return Validation
+     */
+    public function setMessageReposity(ValidationErrorMessage $messageReposity)
+    {
+        $this->messageReposity = $messageReposity;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDatas()
+    {
+        return $this->datas;
+    }
+
+    /**
+     * @param array $datas
+     * @return Validation
+     */
+    public function setDatas($datas)
+    {
+        $this->datas = $datas;
+        return $this;
+    }
+
 }
