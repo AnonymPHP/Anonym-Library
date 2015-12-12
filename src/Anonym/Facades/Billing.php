df9afdfabb62d8df3922937392371920337ba833
anonymframework <anonymframework@gemframework.com>
2015-12-12 18:49:54 +0200

    made some updates

---
+++ src/Anonym/Facades/Billing.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *  This file belongs to AnonymPHP Framework
+ *
+ * @author vahitserifsaglam1 <vahit.serif119@gmail.com>
+ * @website http://anonymphp.com/framework
+ */
+
+namespace Anonym\Facades;
+
+
+use Anonym\Patterns\Facade;
+
+/**
+ * Class Billing
+ * @package Anonym\Facades
+ */
+class Billing extends Facade
+{
+    protected static function getFacadeClass()
+    {
+
+    }
+}
