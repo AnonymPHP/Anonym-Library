8cd83a10f4fecd377b3d682a501d832c709a650d
anonymframework <anonymframework@gemframework.com>
2015-12-19 23:17:29 +0200

    made some updates

--- src/Anonym/Database/Database.php
+++ src/Anonym/Database/Database.php
@@ -12,13 +12,15 @@ use Anonym\Billing\Billing;
 use Anonym\Database\Exceptions\QueryException;
 use Anonym\Support\Arr;
 use ReflectionObject;
+use ArrayAccess;
 use PDO;
+
 
 /**
  * Class Model
  * @package Anonym\Database
  */
-class Database extends Megatron
+class Database extends Megatron implements ArrayAccess
 {
 
     /**
@@ -640,5 +642,67 @@ class Database extends Megatron
     public function __set($name, $value)
     {
         $this->getQueryBuilder()->datas['set'][$name] = $value;
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->first()[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->first()[$offset];
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->first()[$offset]);
     }
 }
