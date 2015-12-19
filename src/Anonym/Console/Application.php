3fb9ad8a295416764bd92ba36461bd938ce87463
anonymframework <anonymframework@gemframework.com>
2015-12-19 12:14:05 +0200

    made some updates

---
+++ src/Anonym/Console/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  This file belongs to AnonymPHP Framework
+ *
+ * @author vahitserifsaglam1 <vahit.serif119@gmail.com>
+ * @website http://anonymphp.com/framework
+ */
+
+namespace Anonym\Console;
+use Anonym\Application\Application as App;
+
+class Application extends App
+{
+
+}
