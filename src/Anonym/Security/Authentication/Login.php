42f61d3c75f80af2c225ab2fbf84c12c44634745
anonymframework <anonymframework@gemframework.com>
2015-12-19 19:28:45 +0200

    made some updates

--- src/Anonym/Security/Authentication/Login.php
+++ src/Anonym/Security/Authentication/Login.php
@@ -80,7 +80,7 @@ class Login extends Authentication implements LoginInterface
             ]);
 
             if ($add->isSuccess()) {
-                return true;
+                return $add;
             }
         }
 
