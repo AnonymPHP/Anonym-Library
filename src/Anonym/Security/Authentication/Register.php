93cb7aaa456ff789a31b9069f7a13d37bdde00f6
anonymframework <anonymframework@gemframework.com>
2015-12-19 18:43:44 +0200

    made some updates

--- src/Anonym/Security/Authentication/Register.php
+++ src/Anonym/Security/Authentication/Register.php
@@ -1,71 +1,68 @@
 <?php
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
+
+namespace Anonym\Security\Authentication;
+
+use Anonym\Database\Database;
+use Anonym\Security\Exception\RegisterArgumentsException;
+
+/**
+ * Class Register
+ * @package Anonym\Security\Authentication
+ */
+class Register extends Authentication implements RegisterInterface
+{
+
+    const USER_FILE = 'user.php';
+
     /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *Sınıfı başlatır
      *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
+     * @param Database $db
+     * @param array $tables
      */
-
-    namespace Anonym\Security\Authentication;
-
-    use Anonym\Database\Base;
-    use Anonym\Database\Mode\Insert;
-    use Anonym\Security\Exception\RegisterArgumentsException;
-
-    /**
-     * Class Register
-     * @package Anonym\Security\Authentication
-     */
-    class Register extends Authentication implements RegisterInterface
+    public function __construct(Database $db, array $tables = [])
     {
+        parent::__construct();
+        $this->setTables($tables);
+        $this->setDb($db->table($tables['table']));
+    }
 
-        const USER_FILE = 'user.php';
+    /**
+     * Kullanıcı kayıt işlemini yapar
+     *
+     * @param array $post
+     * @throws RegisterArgumentsException
+     * @return mixed
+     */
+    public function register(array $post = [])
+    {
+        $tables = $this->getTables();
+        $registerParams = $tables['register'];
+        $tableName = $tables['table'];
 
-        /**
-         *Sınıfı başlatır
-         *
-         * @param Base $db
-         * @param array $tables
-         */
-        public function __construct(Base $db, array $tables = [])
-        {
-            parent::__construct();
-            $this->setTables($tables);
-            $this->setDb($db);
-        }
-
-        /**
-         * Kullanıcı kayıt işlemini yapar
-         *
-         * @param array $post
-         * @throws RegisterArgumentsException
-         * @return mixed
-         */
-        public function register(array $post = [])
-        {
-            $tables = $this->getTables();
-            $registerParams = $tables['register'];
-            $tableName = $tables['table'];
-
-            if (count($registerParams) === count($post)) {
-                $inputValues = array_values($post);
-                if (count(array_diff($inputValues, $registerParams)) > 0) {
-                    throw new RegisterArgumentsException(sprintf('Register parametreleriniz %s dosyasındakilerle aynı olmalıdır.',self::USER_FILE));
-                } else {
-                    $db = $this->getDb();
-                    $insert = $db->insert($tableName, function (Insert $mode) use ($post) {
-                        $mode->set($post)
-                            ->run();
-                    });
-
-                    return ($insert) ? true : false;
-                }
+        if (count($registerParams) === count($post)) {
+            $inputValues = array_values($post);
+            if (count(array_diff($inputValues, $registerParams)) > 0) {
+                throw new RegisterArgumentsException(sprintf('Register parametreleriniz %s dosyasındakilerle aynı olmalıdır.', self::USER_FILE));
             } else {
-                throw new RegisterArgumentsException('Register parametreleriniz %s dosyasındakilerle aynı olmalıdır.', self::USER_FILE);
-            }
+                $db = $this->getDb();
 
+                $insert = $db->insert($post);
+
+                return ($insert->isSuccess()) ? true : false;
+            }
+        } else {
+            throw new RegisterArgumentsException('Register parametreleriniz %s dosyasındakilerle aynı olmalıdır.', self::USER_FILE);
         }
 
     }
+
+}
 
