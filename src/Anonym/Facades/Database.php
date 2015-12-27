21d4807120e7c5a965f9e005ac19b648910dc07d
anonymframework <anonymframework@gemframework.com>
2015-12-27 21:45:35 +0200

    made some updates

--- src/Anonym/Facades/Database.php
+++ src/Anonym/Facades/Database.php
@@ -9,6 +9,7 @@
  */
 
 namespace Anonym\Facades;
+
 use Anonym\Patterns\Facade;
 
 /**
@@ -25,7 +26,7 @@ class Database extends Facade
      */
     protected static function getFacadeClass()
     {
-        return App::make('database.base');
+        return new \Anonym\Database\Database();
     }
 
 }
