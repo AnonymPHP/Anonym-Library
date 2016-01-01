60234d5cb447c0fb148741784905a1155c5d090d
anonymframework <anonymframework@gemframework.com>
2016-01-01 11:52:36 +0200

    made some updates

---
+++ src/Anonym/OAuth/Github.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  This file belongs to AnonymPHP Framework
+ *
+ * @author vahitserifsaglam1 <vahit.serif119@gmail.com>
+ * @website http://anonymphp.com/framework
+ */
+
+namespace Anonym\OAuth;
+
+/**
+ * Class Github
+ * @package Anonym\OAuth
+ */
+class Github
+{
+
+}
