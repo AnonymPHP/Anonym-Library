685f30ce159227dad28998a795ec500c53a814a9
anonymframework <anonymframework@gemframework.com>
2016-01-01 12:33:02 +0200

    made some updates

--- src/Anonym/OAuth/GithubOAuthProviders.php
+++ src/Anonym/OAuth/GithubOAuthProviders.php
@@ -10,6 +10,10 @@ namespace Anonym\OAuth;
 
 
 use Anonym\Application\ServiceProvider;
+use Anonym\Facades\Redirect;
+use Anonym\Facades\Request;
+use Anonym\Facades\Route;
+use Anonym\Facades\Session;
 
 /**
  * Class GithubOAuthProviders
@@ -25,6 +29,24 @@ class GithubOAuthProviders extends ServiceProvider
      */
     public function register()
     {
+        $github = new Github();
 
+        Route::get('/github/login', function () use ($github) {
+            Session::set('state', $state = hash('sha256', microtime(TRUE) . rand() . Request::ip()));
+
+            if (Session::has('access_token')) {
+                Session::delete('access_token');
+            }
+
+            $params = array(
+                'client_id' => OAUTH2_CLIENT_ID,
+                'redirect_uri' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'],
+                'scope' => 'user',
+                'state' => $state
+            );
+            // Redirect the user to Github's authorization page
+
+            Redirect::to('Location: ' . $github->getAuthorizeURL() . '?' . http_build_query($params));
+        });
     }
 }
