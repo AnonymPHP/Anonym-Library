b752913ca8d79a092098102a72ba0fb94972938f
anonymframework <anonymframework@gemframework.com>
2016-01-01 12:24:18 +0200

    made some updates

---
+++ src/Anonym/OAuth/GithubOAuthProviders.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *  This file belongs to AnonymPHP Framework
+ *
+ * @author vahitserifsaglam1 <vahit.serif119@gmail.com>
+ * @website http://anonymphp.com/framework
+ */
+
+namespace Anonym\OAuth;
+
+
+use Anonym\Application\ServiceProvider;
+
+/**
+ * Class GithubOAuthProviders
+ * @package Anonym\OAuth
+ */
+class GithubOAuthProviders extends ServiceProvider
+{
+
+    /**
+     * register the provider
+     *
+     * @return mixed
+     */
+    public function register()
+    {
+
+    }
+}
